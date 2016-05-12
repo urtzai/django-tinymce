07cdfaa8917e7778382be8bc04526e50a8160c7b
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-05-12 11:19:59 +0200

    Template changed to templates

--- tinymce/templates/tinymce/photologue.js
+++ tinymce/templates/tinymce/photologue.js
@@ -1,7 +1,7 @@
 function djangoPhotologue(callback, value, meta){
     if (meta.filetype == 'image') {
 	    tinymce.activeEditor.windowManager.open({
-		    file: '/tinymce/photologue/panel/',
+		    file: '/static/photologue/index.html',// use an absolute path!
 		    title: '{{title}}',
 		    width: 900,  
 		    height: 450,
